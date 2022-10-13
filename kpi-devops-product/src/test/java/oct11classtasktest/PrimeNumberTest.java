5813664e1d1f38091309ac635a84a9605685d85b
piyushbodhani1 <piyush.bodhani@kpipartners.com>
2022-10-13 11:06:06 +0530

    commit for "Prime number"

--- kpi-devops-product/src/test/java/oct11classtasktest/PrimeNumberTest.java
+++ kpi-devops-product/src/test/java/oct11classtasktest/PrimeNumberTest.java
@@ -7,13 +7,25 @@ import org.junit.Test;
 import oct11classtask.PrimeNumber;
 
 public class PrimeNumberTest {
+	//for positive numbers
 	@Test
-	public void testPrime() {
+	public void testPositivePrime() {
 		boolean f1 = PrimeNumber.checkPrime(3);
 		assertEquals(true, f1);
 		boolean f2 = PrimeNumber.checkPrime(4);
 		assertEquals(false, f2);
 		boolean f3 = PrimeNumber.checkPrime(5);
+		assertEquals(true, f3);
+
+	}
+	//for negative numbers
+	@Test
+	public void testNegativePrime() {
+		boolean f1 = PrimeNumber.checkPrime(-3);
+		assertEquals(true, f1);
+		boolean f2 = PrimeNumber.checkPrime(-4);
+		assertEquals(true, f2);
+		boolean f3 = PrimeNumber.checkPrime(-5);
 		assertEquals(true, f3);
 
 	}
